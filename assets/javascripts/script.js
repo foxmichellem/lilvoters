e198d1e66bf6e3a15da095eb0f4c439cadd0726c
Michelle Fox <fox.michelle.m@gmail.com>
2017-05-02 23:55:56 -0700

    add javascript for submit required

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -1,6 +1,9 @@
-// window.onload = function() {
-//   document.querySelector( "form" )
-//       .addEventListener( "invalid", function( event ) {
-//           event.preventDefault();
-//       }, true );
-// }
+window.onload = function() {
+  document.querySelector( "form" )
+      .addEventListener( "invalid", function( event ) {
+          var el = event.target;
+          el.className = 'invalid ';
+          el.placeholder = 'Required';
+          event.preventDefault();
+      }, true );
+}
